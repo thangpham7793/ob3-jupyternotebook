57fba1ccad92dbf3348d0ad9483318ae1d3ccedd
Thang Pham <thangnus@gmail.com>
2020-06-10 01:12:18 +1200

    add time series charts and abstract out chart function

--- chart_helper.py
+++ chart_helper.py
@@ -1,4 +1,7 @@
 import plotly.express as px
+import pandas as pd
+import datetime
+
 
 def quick_sunburst (df, maxdepth=-1):
     columns_list = list(df.columns) 
@@ -17,5 +20,49 @@ def filter_count_by_name(df, name):
                     title= title,
                     width=900)
 
+
+def filters_for_login_chart(df, association=None, status=None):
+    if (association is None) & (status is None):
+        return None
+    elif (association != None) & (status == None):
+        return df['association'] == association
+    elif (association != None) & (status != None):
+        return (df['association'] == association) & (df['status'] == status)
+    else: 
+        return (df['status'] == status)
+
+def make_login_chart(df, title, association=None, status=None, frequency='d', chart_type='bar'):
+    filters = filters_for_login_chart(df, association, status)
+    data = []
+    if filters is None:
+      data = df
+    else:
+      data = df[filters]
+    total_logins_by_frequency = data.set_index('date').resample(frequency).count().reset_index()
+    total_logins_by_frequency = total_logins_by_frequency.rename(columns = {'status' : 'number of logins'})
+
+    if chart_type == 'bar':
+        return px.bar(total_logins_by_frequency, 
+                x = 'date', 
+                y = 'number of logins', 
+                color='number of logins', 
+                color_continuous_scale='teal',
+                color_continuous_midpoint=total_logins_by_frequency['number of logins'].median(),
+                range_color=[0, total_logins_by_frequency['number of logins'].max()],
+                title=title)
+    elif chart_type == 'scatter':
+        return px.scatter(total_logins_by_frequency, 
+                x = 'date', 
+                y = 'number of logins', 
+                color='number of logins', 
+                color_continuous_scale='teal',
+                color_continuous_midpoint=total_logins_by_frequency['number of logins'].median(),
+                range_color=[0, total_logins_by_frequency['number of logins'].max()],
+                title=title)
+    elif chart_type == 'line':
+        return px.line(total_logins_by_frequency, 
+                x = 'date', 
+                y = 'number of logins', 
+                title=title)
 
 
